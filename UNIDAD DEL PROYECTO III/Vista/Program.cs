ab68381cda2a77ed957a22dbe7b0b8c830524f7a
MichelleVides123 <demaryoryorellana2112@gmail.com>
2022-10-11 21:14:28 -0600

    comentario
    ejemplo de comentario

--- UNIDAD DEL PROYECTO III/Vista/Program.cs
+++ UNIDAD DEL PROYECTO III/Vista/Program.cs
@@ -12,6 +12,8 @@ namespace Vista
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            //comentario de ejemplo
         }
     }
 }
